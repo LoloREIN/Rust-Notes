f8ac6124553ef2e6f5d67bccda52ed652dcb8a1c
Lorenzo Reinoso Fuentes <0212511@up.edu.mx>
2024-10-18 12:55:13 -0600

    variable example

--- crash_rust/examples/simple_variable_ex/main.rs
+++ crash_rust/examples/simple_variable_ex/main.rs
@@ -1,12 +1,16 @@
 // Silence some warnings so they don't distract from the exercise.
 #![allow(dead_code, unused_variables)]
 
+mod lib;
+use lib::{on_off, print_array, print_difference, ding};
 fn main() {
     let coords: (f32, f32) = (6.3, 15.0);
     print_difference(coords.0, coords.1);
 
     let coords_arr: [f32;2]  = coords.into();             // create an array literal out of parts of `coord` here
+    let coords_arr_2: [f32;2]  = [coords.0, coords.1];             // create an array literal out of parts of `coord` here
     print_array(coords_arr);        // and pass it in here (this line doesn't need to change)
+    print_array(coords_arr_2);        // and pass it in here (this line doesn't need to change)
 
 
     let series = [1, 1, 2, 3, 5, 8, 13];
@@ -15,6 +19,7 @@ fn main() {
     // "Ding, you found 13!"
     //
     ding(series[series.len()-1]);
+    ding(series[6]);
 
 
 
@@ -42,32 +47,3 @@ fn main() {
     // print_distance(coords);
 }
 
-fn print_difference(x: f32, y: f32) {
-    println!("Difference between {} and {} is {}", x, y, (x - y).abs());
-}
-
-fn print_array(a: [f32; 2]) {
-    println!("The coordinates are ({}, {})", a[0], a[1]);
-}
-
-fn ding(x: i32) {
-    if x == 13 {
-        println!("Ding, you found 13!");
-    }
-}
-
-fn on_off(val: bool) {
-    if val {
-        println!("Lights are on!");
-    }
-}
-
-fn print_distance(z: (f32, f32)) {
-    // Using z.0 and z.1 is not nearly as nice as using x and y.  Lucky for
-    // us, Rust supports destructuring function arguments.  Try replacing "z" in
-    // the parameter list above with "(x, y)" and then adjust the function
-    // body to use x and y.
-    println!(
-        "Distance to the origin is {}",
-        ( z.0.powf(2.0) + z.1.powf(2.0) ).sqrt());
-}
